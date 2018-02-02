780286910b6d56fe25cd6b2ab2d258bf32753b7e
Carlos del Prado <carlos.delprado@11paths.com>
2018-02-02 23:43:56 +0100

    feat: user model with validations

--- starter-code/models/user.model.js
+++ starter-code/models/user.model.js
@@ -1,16 +1,47 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
 
-const userSchema = new Schema({
-  name: String,
-  email: String,
-  password: String,
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required']
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    required: [true, 'Email is required'],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, 'User needs a password']
+  },
   isLaunderer: { type: Boolean, default: false },
   fee: { type: Number, default: null }
+}, { timestamps: true });
+
+userSchema.pre('save', function (next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(SALT_WORK_FACTOR)
+    .then(salt => {
+      bcrypt.hash(user.password, salt)
+        .then(hash => {
+          user.password = hash;
+          next();
+        })
+    })
+    .catch(error => next(error));
 });
 
-userSchema.set('timestamps', true);
-
+userSchema.methods.checkPassword = function (password) {
+  return bcrypt.compare(password, this.password);
+}
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
