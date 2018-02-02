d1423b6d153a8b73ecdca630c154726cdb9d5720
Carlos del Prado <carlos.delprado@11paths.com>
2018-02-03 00:38:31 +0100

    feat: become a launderer

--- starter-code/controllers/laundry.controller.js
+++ starter-code/controllers/laundry.controller.js
@@ -1,5 +1,32 @@
-
+const User = require('../models/user.model');
+const mongoose = require('mongoose');
 
 module.exports.dashboard = (req, res, next) => {
   res.render('laundry/dashboard');
+};
+
+module.exports.doLaunder = (req, res, next) => {
+  const fee = req.body.fee
+  if (!fee) {
+    res.render('laundry/dashboard', {
+      error: { fee: 'Fee is required.'}
+    });
+  } else {
+    const user = req.user;
+    user.fee = fee;
+    user.isLaunder = true;
+    user.save()
+      .then(() => {
+        res.redirect('/dashboard');
+      })
+      .catch(error => {
+        if (error instanceof mongoose.Error.ValidationError) {
+          res.render('laundry/dashboard', {
+            error: error.errors
+          });
+        } else {
+          next(error);
+        }
+      })
+  }
 };
--- starter-code/routes/laundry.routes.js
+++ starter-code/routes/laundry.routes.js
@@ -4,5 +4,6 @@ const laundryController = require('../controllers/laundry.controller');
 const secure = require('../middleware/secure.middleware');
 
 router.get('/dashboard', secure.isAuthenticated, laundryController.dashboard);
+router.post('/launderers', secure.isAuthenticated, laundryController.doLaunder);
 
 module.exports = router;
